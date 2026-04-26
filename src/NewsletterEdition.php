0c91c2e95a3bf5791a3f57de08a6854aa98a407a
lstsystems <admin@lstsystems.com>
2026-04-25 21:44:57 -0400

    Re-enable newsletter status reset after processing

--- src/NewsletterEdition.php
+++ src/NewsletterEdition.php
@@ -157,8 +157,7 @@ class NewsletterEdition
     $this->send_new_edition($nodeId);
 
     //set status back to 0 for future runs
-      // Last disabled to see if this stop the double send
-    //$this->set_newsletter_status($nodeId, '0');
+    $this->set_newsletter_status($nodeId, '0');
   }
 
   /**
